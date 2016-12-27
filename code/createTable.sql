04ad8d5e637bf92c856ce3437b0493518a64b1b6
CZvino <ninja_zfan@126.com>
2016-12-27 22:42:35 +0800

    adjust the code format

--- code/createTable.sql
+++ code/createTable.sql
@@ -18,19 +18,19 @@ create table Advisory
 
 create table AppInformation
 (
-	trackId                             char(15)        PRIMARY KEY,
-	trackName                           nchar(150)      NOT NULL,
+    trackId                             char(15)        PRIMARY KEY,
+    trackName                           nchar(150)      NOT NULL,
     bundleId                            char(150)       NOT NULL,
 
     releaseDate                         smalldatetime   default '1970-01-01 00:00:00',
     userRatingCount                     int             default '0',
     averageUserRating                   decimal(2, 1)   default '0.0',
 
-	currency                            char(5)         default 'None',
+    currency                            char(5)         default 'None',
     formattedPrice                      char(50)        default 'None',
-	price                               smallmoney      default '0',
+    price                               smallmoney      default '0',
 
-	fileSizeBytes                       int             default '0',
+    fileSizeBytes                       int             default '0',
 
     wrapperType                         char(20),
     isGameCenterEnabled                 bit             default 'False',
@@ -44,22 +44,22 @@ create table AppInformation
 
     description                         text,
 
-	languageCodesISO2A                  text,
+    languageCodesISO2A                  text,
 
     version                             char(15),
-	currentVersionReleaseDate           smalldatetime   default '1970-01-01 00:00:00',
+    currentVersionReleaseDate           smalldatetime   default '1970-01-01 00:00:00',
     userRatingCountForCurrentVersion    int             default '0',
     averageUserRatingForCurrentVersion  decimal(2, 1)   default '0.0',
-	releaseNotes                        text,
+    releaseNotes                        text,
 	
-	artistId                            char(15)        NOT NULL,
-	sellerName                          char(150),
+    artistId                            char(15)        NOT NULL,
+    sellerName                          char(150),
 
     trackCensoredName                   nchar(150)		NOT NULL,
     contentAdvisoryRating               char(5),
     trackContentRating                  char(5),
     advisories                          char(50),
 
-	FOREIGN KEY(primaryGenreId)         REFERENCES Genre(genreId),
-	FOREIGN KEY(artistId)               REFERENCES Artist(artistId),
+    FOREIGN KEY(primaryGenreId)         REFERENCES Genre(genreId),
+    FOREIGN KEY(artistId)               REFERENCES Artist(artistId),
 )
